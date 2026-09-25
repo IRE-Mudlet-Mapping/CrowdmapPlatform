d1f1920fdbc1c64bbc92134f006ab6b760818b50
keneanung <keneanung@gmail.com>
2026-09-25 11:46:13 +0200

    fix: avoid top-level await in dangerfile

--- danger/dangerfile.ts
+++ danger/dangerfile.ts
@@ -1,4 +1,4 @@
 import { danger } from "danger";
 import * as rules from "./rules/index.ts";
 
-for (const rule of Object.values(rules)) await rule.check(danger);
+Object.values(rules).forEach((rule) => rule.check(danger));
